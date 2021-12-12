5f06be2d569009382f08ee11781e703c427b8f32
celetor <koncknock@yandex.com>
2021-12-12 10:51:11 +0800

    fix bug

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-python -m nuitka --standalone --mingw64 --show-memory --show-progress  --plugin-enable=qt-plugins --include-qt-plugins=sensible,styles --nofollow-imports --follow-import-to=need --windows-icon-from-ico=resource/connect.ico --include-data-dir=resource=resource --output-dir=o                       --windows-disable-console       main.py
+python -m nuitka --standalone --mingw64 --show-memory --show-progress  --plugin-enable=qt-plugins --include-qt-plugins=sensible,styles --nofollow-imports --follow-import-to=src --windows-icon-from-ico=resource/connect.ico --include-data-dir=resource=resource --output-dir=o                       --windows-disable-console       main.py
 pause
